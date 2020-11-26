5ffdbe47176bf6cbed3fa78d97f0b57fba18b2ef
kanakarn123 <73680257+kanakarn123@users.noreply.github.com>
2020-11-27 02:32:46 +0700

    Update exam.php

--- exam.php
+++ exam.php
@@ -1,22 +1,25 @@
 <?php
+
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'exampleple.mysql.database.azure.com', 'jaooexample@exampleple', 'Jaoo01062544', 'exampleeee', 3306);
+mysqli_real_connect($conn, 'geedy.mysql.database.azure.com', 'geedy@geedy', 'PuiFai2023', 'exam', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 
-$A = $_POST['a'];
-$B = $_POST['b'];
+$A = $_POST['A'];
+$B = $_POST['B'];
+
+
 $sql = "INSERT INTO guestbook (A , B) VALUES ('$A', '$B')";
 
 
 if (mysqli_query($conn, $sql)) {
-  header("Location: showexample.php");
+    header("Location: showexample.php");
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
-  
+
 mysqli_close($conn);
 ?>
