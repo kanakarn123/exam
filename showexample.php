0a213d970e3982427699d9d62c62febd16a306d7
kanakarn123 <73680257+kanakarn123@users.noreply.github.com>
2020-11-27 00:54:29 +0700

    Add files via upload

--- showexample.php
+++ showexample.php
@@ -5,12 +5,12 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'exampleple.mysql.database.azure.com', 'jaooexample@exampleple', 'Jaoo01062544', 'guestbook', 3306);
+mysqli_real_connect($conn, 'exampleple.mysql.database.azure.com', 'jaooexample@exampleple', 'Jaoo01062544', 'exampleeee', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+$res = mysqli_query($conn, 'SELECT * FROM exampleeee');
 ?>
     
 <div class="container">
